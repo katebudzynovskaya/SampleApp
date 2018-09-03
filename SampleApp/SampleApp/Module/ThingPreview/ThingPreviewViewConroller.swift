89180c5235fc7c4bd81762c2f633b1e231e940e1
Kate Budzyanovskaya <k.budzyanovska@gmail.com>
2018-09-03 22:02:41 +0300

    implemented image saving into photo library

--- SampleApp/SampleApp/Module/ThingPreview/ThingPreviewViewConroller.swift
+++ SampleApp/SampleApp/Module/ThingPreview/ThingPreviewViewConroller.swift
@@ -31,4 +31,44 @@ class ThingPreviewViewController: UIViewController {
     @IBAction func closeButtonPressed(_ sender: Any) {
         self.dismiss(animated: true) {}
     }
+    
+}
+
+extension ThingPreviewViewController {
+    
+    @IBAction func previewLondPressed(_ sender: Any) {
+        
+        let alertContoller = UIAlertController.init(title: "Save to Photo Library", message:nil , preferredStyle: .actionSheet)
+        
+        let saveAction = UIAlertAction.init(title: "Save", style: .default) { [weak self] _ in
+            
+            guard let strongSelf = self else { return }
+            guard let image = strongSelf.preview.image else { return }
+            strongSelf.save(image: image)
+        }
+        
+        alertContoller.addAction(saveAction)
+        alertContoller.addAction(UIAlertAction.init(title: "Cancel", style: .cancel))
+        present(alertContoller, animated: true) {}
+    }
+    
+    func save(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_: didFinishSavingWithError: contextInfo:)), nil)
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        
+        if let error = error {
+            
+            let alertContoller = UIAlertController(title: "Saving error", message: error.localizedDescription, preferredStyle: .alert)
+            alertContoller.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alertContoller, animated: true)
+        } else {
+            
+            let alertContoller = UIAlertController(title: "Saved", message: "Image saved to the Photo Library", preferredStyle: .alert)
+            alertContoller.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alertContoller, animated: true)
+        }
+    }
+    
 }
